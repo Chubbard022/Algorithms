699e9669afe372ce8e3bd5d8315303db3c660e65
Chubbard022 <chubbard022@gmail.com>
2019-07-17 16:08:53 -0700

    acidental push to eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,17 +8,7 @@ import sys
 
 
 def eating_cookies(n, cache=None):
-  if n == 0:
-    return 1
-  elif n < 3:
-    return n
-  elif cache and cache[n] > 0:
-    return cache[n]
-  else:
-    if not cache:
-      cache = {i: 0 for i in range(n+1)}
-    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
-    return cache[n]
+  pass
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
