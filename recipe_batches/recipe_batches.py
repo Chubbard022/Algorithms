e3b4a5fe5bc225690ad10393642b661c496f971a
Chubbard022 <chubbard022@gmail.com>
2019-07-17 20:50:52 -0700

    completed recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,31 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  count = []
+  batch_count = 0
+
+  if len(recipe) != len(ingredients):
+    return batch_count
+
+  for ingredient in recipe:
+    count.append(ingredients[ingredient]//recipe[ingredient])
+  
+  batch_count = min(count)
+  return batch_count
+  # for item in recipe:
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+# batch = []
+# num = { 'milk': 2, 'sugar': 40, 'butter': 20 }
+
+# ing =  { 'milk': 5, 'sugar': 120, 'butter': 500 }
+
+# for item in num:
+#  batch.append(ing[item]//num[item])
+
+# print(batch)
+
+# print(min(batch))
+
+
